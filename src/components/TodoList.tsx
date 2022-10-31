874f19388ef4fee49743562033da44239207de5a
Aleksei Treshchalin <treshchal@gmail.com>
2022-10-31 14:28:09 +0300

    Edit TodoList component
    Add todoEntries state
    Replace TodoEntry component with list of entries

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,23 @@
+import React from "react";
+// Components
 import TodoEntry from "./TodoEntry";
 import TodoForm from "./TodoForm";
+// Types
+import { TodoEntryInterface } from "../utility/interfaces";
 
 const TodoList = () => {
+  const [todoEntries, setTodoEntries] = React.useState<TodoEntryInterface[]>(
+    []
+  );
+
+  const todoEntriesElements = todoEntries.map((todoEntry) => {
+    return <TodoEntry />;
+  });
+
   return (
     <main>
       <TodoForm />
-      <TodoEntry />
+      {todoEntriesElements}
     </main>
   );
 };
