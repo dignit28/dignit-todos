060d4b0f7be93274ab427eab249ec70f3f2db654
Aleksei Treshchalin <treshchal@gmail.com>
2022-10-31 14:34:52 +0300

    Add default entries
    Add default data for visual and testing purposes
    Replace default state value with new data

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,11 +4,12 @@ import TodoEntry from "./TodoEntry";
 import TodoForm from "./TodoForm";
 // Types
 import { TodoEntryInterface } from "../utility/interfaces";
+// Data
+import { defaultTodoEntries } from "../defaults/defaultData";
 
 const TodoList = () => {
-  const [todoEntries, setTodoEntries] = React.useState<TodoEntryInterface[]>(
-    []
-  );
+  const [todoEntries, setTodoEntries] =
+    React.useState<TodoEntryInterface[]>(defaultTodoEntries);
 
   const todoEntriesElements = todoEntries.map((todoEntry) => {
     return <TodoEntry />;
---
+++ src/defaults/defaultData.ts
@@ -0,0 +1,11 @@
+import { TodoEntryInterface } from "../utility/interfaces";
+
+export const defaultTodoEntries: TodoEntryInterface[] = [
+  { content: "test1", completed: false },
+  { content: "test2", completed: true },
+  {
+    content: "test234 8971fb23847 2134 019                12342sdgah",
+    completed: true,
+  },
+  { content: "teeeeeeeeeeeeeeeeeeeeeeeeeeee", completed: false },
+];
