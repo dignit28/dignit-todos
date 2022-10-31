0eb3d5fe5b7c63dbac1c6e9ac885f6d4008eb255
Aleksei Treshchalin <treshchal@gmail.com>
2022-10-31 14:12:50 +0300

    Edit TodoForm component
    Add input state
    Add controlled input
    Add button click

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+
 const TodoForm = () => {
-  return <div>TodoForm</div>;
+  const [inputData, setInputData] = React.useState("");
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setInputData(event.target.value);
+  };
+
+  const handleButtonClick = (event: React.SyntheticEvent): void => {
+    event.preventDefault();
+    console.log(inputData);
+  };
+
+  return (
+    <form onSubmit={handleButtonClick}>
+      <input onChange={handleInputChange} value={inputData}></input>
+      <button>+</button>
+    </form>
+  );
 };
 
 export default TodoForm;
